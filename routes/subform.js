c8dc218b691ad08f7ab8415218737dc53d3e6068
liugu <liuguww@qq.com>
2015-09-11 16:01:17 +0800

    study

---
+++ routes/subform.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('subform', { title: '提交表单及接收参数示例' });
+});
+
+module.exports = router;
